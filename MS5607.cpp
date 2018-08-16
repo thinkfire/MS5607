10fcc8966f2f76c0241dce33a7861983b16523e0
Amit <amitate007@gmail.com>
2018-08-16 19:16:30 +0530

    fixed

--- MS5607.cpp
+++ MS5607.cpp
@@ -116,7 +116,7 @@ char MS5607::startMeasurment(void){
 
 
 char MS5607::readDigitalValue(void){
-    if(readUInt_32(char(CONV_D1), DP) && readUInt_32(char(CONV_D2), DT)){
+    if(readUInt_32(CONV_D1, DP) && readUInt_32(CONV_D2, DT)){
       Serial.println(DP);
       Serial.println(DT);
       return(1);
