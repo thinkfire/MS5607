104a88bacc5a3b6a5bc4385ac5db1ab96a7ebae1
Amit <amitate007@gmail.com>
2018-08-16 13:54:43 +0530

    bug fixed

--- MS5607.cpp
+++ MS5607.cpp
@@ -24,12 +24,12 @@ char MS5607::begin()
 char MS5607::readCalibration()
 {
   if(
-    readUInt(PROM_READ+2, C1);
-    readUInt(PROM_READ+2*2, C2);
-    readUInt(PROM_READ+2*3, C3);
-    readUInt(PROM_READ+2*4, C4);
-    readUInt(PROM_READ+2*5, C5);
-    readUInt(PROM_READ+2*6, C6);
+    readUInt(PROM_READ+2, C1) &&
+    readUInt(PROM_READ+2*2, C2) &&
+    readUInt(PROM_READ+2*3, C3) &&
+    readUInt(PROM_READ+2*4, C4) &&
+    readUInt(PROM_READ+2*5, C5) &&
+    readUInt(PROM_READ+2*6, C6) &&
   ){
     Serial.println(C1,2);
     Serial.println(C2,2);
