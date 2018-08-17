2a09b9009644f639cd4580f7cb64e23f12503e6c
Amit <amitate007@gmail.com>
2018-08-17 19:25:36 +0530

    Fixed all and code is working

--- MS5607.h
+++ MS5607.h
@@ -3,13 +3,16 @@
   TE Connectivity sensor pressure sensor MS5607 library for the Arduino microcontroller
   this library used I2C bus.
 */
+//#define MS5607_DEBUG
+
 #define MS5607_ADDR 0X76
 #define PROM_READ  0xA0
 #define RESET  0x1E
-#define CONV_D1  0x40 //OSR = 256 //calculation time = 0.60 ms
-//#define CONV_D1 0x48 //OSR = 4096 //calculation time = 9.04 ms
-#define CONV_D2  0x50 //OSR = 256 //calculation time = 0.60 ms
-//#define CONV_D2 0x58 //OSR=4096 //calculation time = 9.04 ms
+#define OSR 4096
+//#define CONV_D1  0x40 //OSR = 256 //calculation time = 0.60 ms
+#define CONV_D1 0x48 //OSR = 4096 //calculation time = 9.04 ms
+//#define CONV_D2  0x50 //OSR = 256 //calculation time = 0.60 ms
+#define CONV_D2 0x58 //OSR=4096 //calculation time = 9.04 ms
 #define R_ADC  0X00 //adc read
 
 #include <Arduino.h>
@@ -19,26 +22,25 @@ class MS5607
   public:
     MS5607();
     char begin();
-    char getTemperature(void);
-    char getPressure(void);
-    char getDigitalValue(void);
-
-
+    float getTemperature(void);
+    float getPressure(void);
+    
   private:
     unsigned int C1,C2,C3,C4,C5,C6;
     unsigned long DP, DT;
-    long dT, TEMP;
-    double OFF, SENS, P;
+    float dT, TEMP, P;
+    float const1, const2, const4, const5, const6;
+    int64_t const3;
+    int64_t OFF, SENS;
 
-    char readCalibration();
     char resetDevice(void);
-    char readDigitalValue(unsigned long &value);
-    char startMeasurment(void);
-    //char readDigitalValue(void);
+    char readCalibration();
     char readUInt_16(char address, unsigned int &value);
-    char readUInt_32(char address, unsigned long &value);
     char readBytes(unsigned char *values, char length);
-    char writeBytes(unsigned char *values, char length);
-
+    char startConversion(char CMD);
+    char startMeasurment(void);
+    char getDigitalValue(void);
+    char readDigitalValue(unsigned long &value);
+    String ToString(uint64_t x);
 
 };
